04931fe51bcb19ad50ae5ed0c583d568176bfb36
tajaa <tajatheprince@gmail.com>
2024-06-18 12:48:39 -0700

    added a margin

--- todo.c
+++ todo.c
@@ -1,14 +1,15 @@
 #include <GLFW/glfw3.h>
 #include <leif/leif.h>
 
-
+#define WIN_MARGIN 20.0f
+static int winw = 1280, winh = 720 ;
 int main() {
   glfwInit();
-  GLFWwindow* window = glfwCreateWindow(1280, 720, "Todo", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(winw, winh,"Todo", NULL, NULL);
 
   glfwMakeContextCurrent(window);
 
-  lf_init_glfw(1280, 720, window);
+  lf_init_glfw(winw, winh, window);
 
   
   while(!glfwWindowShouldClose(window)){
@@ -16,6 +17,12 @@ int main() {
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
     lf_begin();
+
+    lf_div_begin(((vec2s){WIN_MARGIN, WIN_MARGIN}), ((vec2s){winw- WIN_MARGIN
+          * 2.0f, winh - WIN_MARGIN * 2.0f}),true);
+    
+
+
     lf_text("it works!");
     lf_end();
 
