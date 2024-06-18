47890e8a283af2f1dcd70aa52580ff6259bd1dc5
tajaa <tajatheprince@gmail.com>
2024-06-18 00:48:45 -0700

    added leif and window

--- todo.c
+++ todo.c
@@ -1,30 +1,31 @@
 #include <GLFW/glfw3.h>
+#include <leif/leif.h>
+
 
 int main() {
-  //init GLFW
   glfwInit();
-  //create a window w/ a width of 1280 px, 720 pixels height, 
-  //&little "todo" and last 2 params for spec a mointor adn OpenGL context
-  // -sharing which are not used here yet
   GLFWwindow* window = glfwCreateWindow(1280, 720, "Todo", NULL, NULL);
+
+  glfwMakeContextCurrent(window);
+
+  lf_init_glfw(1280, 720, window);
+
   
   while(!glfwWindowShouldClose(window)){
-    //clear the color buffer
     glClear(GL_COLOR_BUFFER_BIT);
-    //set the clear color to a dark gray w/alpha of 1
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
-    // Poll for events (keyboard, mouse, etc)
+    lf_begin();
+    lf_text("it works!");
+    lf_end();
+
     glfwPollEvents();
-    //swap the front and back buffers
+
     glfwSwapBuffers(window);
 
   }
-   //destory the window  
     glfwDestroyWindow(window);
-    //terminate GLFW
     glfwTerminate();
-    //return 0 to indicate a successful program execution
     return 0;
   
   
