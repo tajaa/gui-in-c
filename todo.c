ffc2d7897372e4eb2c5c43aa88bd793a22391884
tajaa <tajatheprince@gmail.com>
2024-06-20 11:24:58 -0700

    moved everything for the top bar into its own static function and rendering it later

--- todo.c
+++ todo.c
@@ -4,6 +4,32 @@
 #define WIN_MARGIN 20.0f
 static int winw = 1280, winh = 720 ;
 static LfFont titlefont;
+
+static void rendertopbar()
+{
+  lf_push_font(&titlefont);
+    lf_text("Your to do");
+    lf_pop_font();
+{
+      const float width = 160.0f;
+      //sets pointer for next element to a given value
+      lf_set_ptr_x_absolute(winw - width - WIN_MARGIN * 2.0f);
+      LfUIElementProps props = lf_get_theme().button_props;
+      props.margin_left = 0.0f;
+      props.margin_right= 0.0f;
+
+      props.color = (LfColor){65, 167, 204, 255};
+      props.border_width =0.0f; props.corner_radius = 4.0f;
+
+      lf_push_style_props(props);
+      lf_set_line_should_overflow(false);
+      lf_button_fixed("new task!", width, -1);
+      lf_set_line_should_overflow(true);
+      lf_pop_style_props();
+    }
+
+}
+
 int main() {
   glfwInit();
   GLFWwindow* window = glfwCreateWindow(winw, winh,"Todo", NULL, NULL);
@@ -27,24 +53,8 @@ int main() {
 
     lf_div_begin(((vec2s){WIN_MARGIN, WIN_MARGIN}), ((vec2s){winw- WIN_MARGIN
           * 2.0f, winh - WIN_MARGIN * 2.0f}),true); 
-
-    lf_push_font(&titlefont);
-    lf_text("Your to do!");
-    lf_pop_font();
-    {
-      const float width = 160.0f;
-      //sets pointer for next element to a given value
-      lf_set_ptr_x_absolute(winw - width - WIN_MARGIN * 2.0f);
-      LfUIElementProps props = lf_get_theme().button_props;
-      props.margin_left = 0.0f;
-      props.margin_right= 0.0f;
-
-      lf_push_style_props(props);
-      lf_set_line_should_overflow(false);
-      lf_button_fixed("new task!", width, -1);
-      lf_set_line_should_overflow(true);
-      lf_pop_style_props();
-    }
+    
+  rendertopbar();
 
 
     lf_end();
